42f7e48e7788702cd01e90420d23d0dcbaf349f4
Quentin Little <q10@areUXperienced.com>
2013-01-22 15:46:09 -0800

    First init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quentify::Application.config.secret_token = 'ddd8ef307311fa381fe61ebd4810733d39e75f6c6b399c2a483b9675ab5b523e758a8daa232c5e9a9bb5cc8e7cbcdeeb4fbdee0763d72b6eff1718cb08433087'
